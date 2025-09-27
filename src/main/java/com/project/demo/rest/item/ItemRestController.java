3199adcb263e03f9acca16a17fd3597bbb0d165e
sarrietag <sarrietag@ucenfotec.ac.cr>
2025-09-26 18:57:01 -0600

    Item Controller Mapping Complete
    Item Controller Mapping Complete

--- src/main/java/com/project/demo/rest/item/ItemRestController.java
+++ src/main/java/com/project/demo/rest/item/ItemRestController.java
@@ -1,29 +1,52 @@
 package com.project.demo.rest.item;
 
 import com.project.demo.logic.entity.game.Game;
-import com.project.demo.logic.entity.game.GameRepository;
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.logic.entity.http.Meta;
 import com.project.demo.logic.entity.items.Item;
 import com.project.demo.logic.entity.items.ItemRepository;
+import com.project.demo.logic.entity.order.Order;
+import com.project.demo.logic.entity.rol.Role;
+import com.project.demo.logic.entity.rol.RoleEnum;
 import com.project.demo.logic.entity.user.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/item")
+@RequestMapping("/items")
 public class ItemRestController {
     @Autowired
     private ItemRepository itemRepository;
 
+
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN', 'USER')")
-    public List<Item> getAllItems(){
-        return itemRepository.findAll();
+    public ResponseEntity<?> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<Item> ordersPage = itemRepository.findAll(pageable);
+        Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+        meta.setTotalPages(ordersPage.getTotalPages());
+        meta.setTotalElements(ordersPage.getTotalElements());
+        meta.setPageNumber(ordersPage.getNumber() + 1);
+        meta.setPageSize(ordersPage.getSize());
+
+        return new GlobalResponseHandler().handleResponse("Order retrieved successfully",
+                ordersPage.getContent(), HttpStatus.OK, meta);
     }
-
-
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     public Item updateItem(@PathVariable Long id, @RequestBody Item item) {
@@ -41,16 +64,42 @@ public class ItemRestController {
                 });
     }
 
-    @PostMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
-    public Item addItem(@RequestBody Item item) {
-        return  itemRepository.save(item);
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<?> patchItem(@PathVariable Long itemId, @RequestBody Item item, HttpServletRequest request) {
+        Optional<Item> foundItem = itemRepository.findById(itemId);
+        if(foundItem.isPresent()) {
+            if(item.getName() != null) foundItem.get().setName(item.getName());
+            if(item.getDescription() != null) foundItem.get().setDescription(item.getDescription());
+            if(item.getPrice() != null) foundItem.get().setStatus(String.valueOf(item.getPrice()));
+            if(item.getStatus() != null) foundItem.get().setStatus(item.getStatus());
+            itemRepository.save(foundItem.get());
+            return new GlobalResponseHandler().handleResponse("Order updated successfully",
+                    foundItem.get(), HttpStatus.OK, request);
+        } else {
+            return new GlobalResponseHandler().handleResponse("Order id " + itemId + " not found"  ,
+                    HttpStatus.NOT_FOUND, request);
+        }
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
-    @DeleteMapping("/{id}")
-    public void deleteItem (@PathVariable Long id) {
-        itemRepository.deleteById(id);
+
+    @PostMapping("/item")
+    public ResponseEntity<?> addItem(@RequestBody Item item, HttpServletRequest request) {
+        Item savedItem = itemRepository.save(item);
+        return new GlobalResponseHandler().handleResponse("Item created successfully",
+                savedItem, HttpStatus.CREATED, request);
     }
 
+    @DeleteMapping("/{itemId}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<?> deleteItem(@PathVariable Long itemId, HttpServletRequest request) {
+        Optional<Item> foundItem = itemRepository.findById(itemId);
+        if(foundItem.isPresent()) {
+            itemRepository.deleteById(foundItem.get().getId());
+            return new GlobalResponseHandler().handleResponse("Item deleted successfully",
+                    foundItem.get(), HttpStatus.OK, request);
+        } else {
+            return new GlobalResponseHandler().handleResponse("Item id " + itemId + " not found"  ,
+                    HttpStatus.NOT_FOUND, request);
+        }
+    }
 }
